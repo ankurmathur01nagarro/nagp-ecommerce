d6e6983106d78b7b447bb8179db98fa2a0e645c8
Ankur Mathur <ankur.mathur01@nagarro.com>
2025-12-26 23:59:05 +0530

    nav bar component complete

--- ui/src/app/components/nav-bar-component/nav-bar-component.ts
+++ ui/src/app/components/nav-bar-component/nav-bar-component.ts
@@ -1,74 +1,45 @@
-import { Component, ElementRef, ViewChild, type AfterViewInit, type OnInit, type TemplateRef } from '@angular/core';
+import { Component, ElementRef, type AfterViewInit, forwardRef, input, viewChild, contentChildren, contentChild, signal, computed, afterRenderEffect } from '@angular/core';
 import { NgTemplateOutlet } from "@angular/common";
-
-type NavBarItemData = {
-  label: string;
-  link?: string;
-  icon?: string;
-}
-
-type SimpleNavBarItem = NavBarItemData & {
-  subItemType: 'simple';
-  subItems?: NavBarItemData[];
-};
-
-type TabularNavBarItem = NavBarItemData & {
-  subItemType: 'tabular';
-  columns: {
-    header: string;
-    size: number;
-    items: NavBarItemData[];
-  }[];
-}
-
-type MegaNavBarItem = NavBarItemData & {
-  subItemType: 'mega';
-  template: TemplateRef<any>;
-}
-
-type NavBarItem = SimpleNavBarItem | TabularNavBarItem | MegaNavBarItem;
 
 @Component({
   selector: 'App-NavBar',
   templateUrl: './nav-bar-component.html',
   styleUrl: './nav-bar-component.css',
-  imports: [NgTemplateOutlet],
+  imports: [forwardRef(() => NavMenu), forwardRef(() => NavItem), forwardRef(() => SubMenu), forwardRef(() => CustomSubMenu), forwardRef(() => SubMenuColumn)],
 })
-export class NavBarComponent implements OnInit, AfterViewInit {
-  @ViewChild('mainMenu') mainMenu!: ElementRef<HTMLElement>;
-  @ViewChild('megaTemplate', { static: true }) megaTemplate!: TemplateRef<any>;
+export class NavBarComponent {
 
-  navBarItemCollection: NavBarItem[] = [];
+}
 
-  ngOnInit(): void {
-    this.navBarItemCollection = [
-      { label: 'Home', subItemType: 'simple', icon: 'icon-screen-desktop', link: '/' },
-      { label: 'Categories', subItemType: 'simple', icon: 'icon-bag', subItems: [
-        { label: 'Electronics', icon:'icon-screen-smartphone', link: '/categories/electronics' },
-        { label: 'Books', link: '/categories/books' },
-        { label: 'Clothing', link: '/categories/clothing' },
-      ]},
-      { label: 'Deals', subItemType: 'tabular', link: '/deals', icon: 'icon-tag', columns: [
-        {
-          header: 'Today\'s Deals',
-          size: 6,
-          items: [
-            { label: 'Electronics Deals', link: '/deals/electronics' },
-            { label: 'Fashion Deals', link: '/deals/fashion' },
-          ]
-        },
-        {
-          header: 'Upcoming Deals',
-          size: 6,
-          items: [
-            { label: 'Holiday Deals', link: '/deals/holiday' },
-            { label: 'Clearance Sales', link: '/deals/clearance' },
-          ]
-        }],
-      },
-      { label: 'Mega Menu', subItemType: 'mega', icon: 'icon-grid', template: this.megaTemplate },
-      { label: 'Contact Us', subItemType: 'simple', icon: 'icon-call-in', link: '/contact' },
-    ];
+@Component({
+  selector: 'div[App-NavMenu]',
+  host: {
+    '[class.top-menu]': 'true',
+  },
+  template: `
+    <!-- main menu begin -->
+    <div class="container">
+      <nav #mainMenu class="main-menu">
+        <ul class="nav-menu">
+          <ng-content />
+        </ul>
+      </nav>
+    </div>
+    <!-- main menu end -->`,
+})
+export class NavMenu implements AfterViewInit {
+  mainMenu = viewChild.required('mainMenu', { read: ElementRef<HTMLElement> });
+  subMenuItems = contentChildren(NavItem);
+
+  constructor() {
+    afterRenderEffect(() => {
+      if (this.subMenuItems().length > 0) {
+        // Logic to handle submenu items
+        this.subMenuItems().forEach(item => {
+          item.isNavItem.set(true);
+        });
+      }
+    });
   }
 
   ngAfterViewInit(): void {
@@ -78,7 +49,7 @@ export class NavBarComponent implements OnInit, AfterViewInit {
   }
 
   private initializeMegaMenu(): void {
-    jQuery(this.mainMenu.nativeElement).accessibleMegaMenu({
+    jQuery(this.mainMenu().nativeElement).accessibleMegaMenu({
       /* prefix for generated unique id attributes, which are required
          to indicate aria-owns, aria-controls and aria-labelledby */
       uuidPrefix: "accessible-megamenu",
@@ -98,4 +69,109 @@ export class NavBarComponent implements OnInit, AfterViewInit {
       openClass: "open"
     });
   }
+}
+
+@Component({
+  selector: 'li[App-NavItem]',
+  host: {
+    '[class.nav-item]': 'isNavItem()',
+    '[class.has-sub-nav]': 'isSubMenu() && hasSubItems()',
+  },
+  template: `
+    <ng-container>
+      <a [href]="link() || '#'">
+        @if (icon()) {
+          <span><i [class]="icon()">&nbsp;</i>{{label()}}</span>
+        } @else {
+          <span>{{label()}}</span>
+        }
+      </a>
+      <ng-content />
+    </ng-container>
+  `,
+})
+export class NavItem {
+  label = input.required<string>();
+  link = input.required<string>();
+  icon = input<string>();
+
+  isSubMenu = signal(false);
+  isNavItem = signal(false);
+  childSubMenu = contentChild(SubMenu, { descendants: false });
+  hasSubItems = computed(() => {
+    const subMenu = this.childSubMenu();
+    return !!subMenu;
+  });
+}
+
+@Component({
+  selector: 'div[App-SubMenu]',
+  host: {
+    '[class.sub-nav]': 'true',
+  },
+  template: `
+    <ul class="sub-nav-group">
+      <ng-content />
+    </ul>
+  `,
+})
+export class SubMenu {
+  subMenuItems = contentChildren(NavItem);
+
+  constructor() {
+    afterRenderEffect(() => {
+      if (this.subMenuItems().length > 0) {
+        // Logic to handle submenu items
+        this.subMenuItems().forEach(item => {
+          item.isSubMenu.set(true);
+        });
+      }
+    });
+  }
+}
+
+@Component({
+  selector: 'div[App-CustomSubMenu]',
+  host: {
+    '[class]': "'sub-nav full padding'",
+  },
+  template: `
+    <ng-template #content>
+      <ng-content></ng-content>
+    </ng-template>
+    @if (type() === 'tabular') {
+      <div class="row">
+        <ng-container *ngTemplateOutlet="content"></ng-container>
+      </div>
+    } @else {
+      <ng-container *ngTemplateOutlet="content"></ng-container>
+    }
+  `,
+  imports: [NgTemplateOutlet],
+})
+export class CustomSubMenu {
+  type = input.required<"tabular" | "custom">();
+}
+
+@Component({
+  selector: 'div[App-SubMenuColumn]',
+  host: {
+    '[class]': 'columnSize()',
+  },
+  template: `
+    <ng-container>
+      <h3 class="sub-nav-title">{{header()}}</h3>
+      <ul class="sub-nav-group sub-nav-grey">
+        <ng-content></ng-content>
+      </ul>
+    </ng-container>
+  `,
+})
+export class SubMenuColumn {
+  header = input.required<string>();
+  size = input.required<number>();
+
+  columnSize = computed(() => `col-xs-${this.size()}`);
+
+  items = contentChildren(NavItem);
 }
